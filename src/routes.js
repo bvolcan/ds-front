f7f682c459fc393aabde4041c4c4a1dc8e9d67d6
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2023-04-10 16:32:40 -0300

    feat: change password screen

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Login from './containers/Login'
+import ChangePassword from './containers/Login/ChangePassword'
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
+				<Route component={ChangePassword} path='/changepassword' />
 				<Route component={Login} path='/' />
 			</Switch>
 		</BrowserRouter>
