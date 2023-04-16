0c04456d77776ba5dcbf21cb0899461811814900
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2023-04-16 14:15:11 -0300

    feat: working on view revision page

--- src/components/ShowRevision/ShowRevision.js
+++ src/components/ShowRevision/ShowRevision.js
@@ -1,153 +1,106 @@
 import React from 'react'
 import './ShowRevision.css'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
-const ShowRevision = () => {
+const ShowRevision = ({ data }) => {
+	// console.log(data)
 	return (
 		<div className='show-revision'>
-			<h2>Revisores</h2>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
-			</p>
+			<div className='show-revision-title'>
+				<h2>Revisão - Professor {data.numeroServidorRevisor}</h2>
+			</div>
+			<div className='show-revision-content'>
+				<div className='objective-criteria'>
+					<h3>Critérios Objetivos</h3>
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 1200 }} aria-label='criterios objetivos'>
+							<TableBody>
+								<TableRow
+									key='Apresentacao e consistencia'
+									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+								>
+									<TableCell
+										component='th'
+										scope='row'
+										align='left'
+										width='50%'
+									>
+										Apresentação e consistência
+									</TableCell>
+									<TableCell>{data.apresentacao}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Relevância e contribuição</TableCell>
+									<TableCell>{data.relevancia}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Metodologia</TableCell>
+									<TableCell>{data.metodologia}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Adequação ao curso</TableCell>
+									<TableCell>
+										{data.foiAdequado ? '1 - Adequado' : '2 - Inadequado'}
+									</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Plano de trabalho a ser desenvolvido</TableCell>
+									<TableCell>{data.planoDeTrabalho}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Avaliação geral</TableCell>
+									<TableCell>
+										{data.foiAprovado
+											? '1 - Proposta Aprovada'
+											: '2 - Proposta Reprovada'}
+									</TableCell>
+								</TableRow>
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</div>
+				<div className='dissertative-criteria'>
+					<h3>Critérios Dissertativos</h3>
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 1200 }} aria-label='criterios dissertativos'>
+							<TableBody>
+								<TableRow
+									key='Resumo da proposta'
+									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+								>
+									<TableCell
+										component='th'
+										scope='row'
+										align='left'
+										width='50%'
+									>
+										Resumo da proposta
+									</TableCell>
+									<TableCell>{data.resumo}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Pontos fortes da proposta</TableCell>
+									<TableCell>{data.pontosFortes}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>´Pontos fracos da proposta</TableCell>
+									<TableCell>{data.pontosFracos}</TableCell>
+								</TableRow>
+								<TableRow>
+									<TableCell>Detalhamento da avaliação</TableCell>
+									<TableCell>{data.detalhamento}</TableCell>
+								</TableRow>
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/containers/Student/ViewRevision/index.js
+++ src/containers/Student/ViewRevision/index.js
@@ -2,6 +2,41 @@ import React from 'react'
 import { Header, ShowRevision } from '../../../components'
 import './style.css'
 
+let revisoesData = [
+	{
+		id: 1,
+		propostaId: 1,
+		revisadoEm: '16-04-2023',
+		apresentacao: '3 - Bom',
+		relevancia: '4 - Excelente',
+		metodologia: '3 - Bom',
+		foiAdequado: true,
+		planoDeTrabalho: '2 - Exequível',
+		foiAprovado: true,
+		resumo: 'O trabalho trata sobre xxxx.',
+		pontosFortes: 'Os pontos fortes são que blablabla.',
+		pontosFracos: 'Como pontos fracos é possivel citar xlaxaoxalxoalo.',
+		detalhamento: 'Detalhes não sei pois não li tudo.',
+		numeroServidorRevisor: 1234
+	},
+	{
+		id: 2,
+		propostaId: 1,
+		revisadoEm: '16-04-2023',
+		apresentacao: '2 - Regular',
+		relevancia: '3 - Bom',
+		metodologia: '1 - Insuficiente',
+		foiAdequado: false,
+		planoDeTrabalho: '1 - Mal especificado e/ou não exequível',
+		foiAprovado: false,
+		resumo: 'O trabalho trata sobre um bagulho interessante mas foi mal feito.',
+		pontosFortes: 'Os pontos fortes são o assunto né mas ta uma bosta.',
+		pontosFracos: 'Os pontos fracos são todo o resto.',
+		detalhamento: 'Nao sei detalhes pois li rapidao e nao entendi nada.',
+		numeroServidorRevisor: 1256
+	}
+]
+
 let propostaTitle = 'Proposta Teste'
 const ViewRevision = () => {
 	return (
@@ -12,7 +47,9 @@ const ViewRevision = () => {
 				<h3>Proposta: {propostaTitle}</h3>
 			</div>
 			<div className='view-revision-box'>
-				<ShowRevision />
+				{revisoesData.map((revisao) => (
+					<ShowRevision data={revisao} />
+				))}
 			</div>
 		</div>
 	)
