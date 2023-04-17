a3e0f9987402176c758d362b2e3227ea81bccb1e
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2023-04-17 17:00:57 -0300

    feat: ShowRevision component now handles both tables type and synced with json style from the backend, also added date-fns to style date

--- src/components/ShowRevision/ShowRevision.js
+++ src/components/ShowRevision/ShowRevision.js
@@ -6,117 +6,165 @@ import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
+import { format } from 'date-fns'
 
-const ShowRevision = ({ data }) => {
+const ShowRevision = ({ isRevision, data }) => {
 	// console.log(data)
+
 	return (
-		<div className='show-revision'>
-			<div className='show-revision-title'>
-				<h2>Revisão - Professor {data.numeroServidorRevisor}</h2>
-			</div>
-			<div className='show-revision-content'>
-				<div className='objective-criteria'>
-					<h3>Critérios Objetivos</h3>
-					<TableContainer component={Paper}>
-						<Table sx={{ width: 1200 }} aria-label='criterios objetivos'>
-							<TableBody>
-								<TableRow
-									key='Apresentacao e consistencia'
-									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+		<>
+			{isRevision ? (
+				<div className='show-revision'>
+					<div className='show-revision-title'>
+						<h2>Revisão - Professor {data.numeroServidorRevisor}</h2>
+					</div>
+					<div className='show-revision-content'>
+						<div className='objective-criteria'>
+							<h3>Critérios Objetivos</h3>
+							<TableContainer component={Paper}>
+								<Table sx={{ width: 1200 }} aria-label='criterios objetivos'>
+									<TableBody>
+										<TableRow
+											key='Apresentacao e consistencia'
+											sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+										>
+											<TableCell
+												component='th'
+												scope='row'
+												align='left'
+												width='50%'
+												style={{ fontWeight: 600 }}
+											>
+												Apresentação e consistência
+											</TableCell>
+											<TableCell>{data.presentation}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Relevância e contribuição
+											</TableCell>
+											<TableCell>{data.relevance}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Metodologia
+											</TableCell>
+											<TableCell>{data.methodology}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Adequação ao curso
+											</TableCell>
+											<TableCell>
+												{data.wasAdequate ? '1 - Adequado' : '2 - Inadequado'}
+											</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Plano de trabalho a ser desenvolvido
+											</TableCell>
+											<TableCell>{data.workPlan}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Avaliação geral
+											</TableCell>
+											<TableCell>
+												{data.wasApproved
+													? '1 - Proposta Aprovada'
+													: '2 - Proposta Reprovada'}
+											</TableCell>
+										</TableRow>
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</div>
+						<div className='dissertative-criteria'>
+							<h3>Critérios Dissertativos</h3>
+							<TableContainer component={Paper}>
+								<Table
+									sx={{ width: 1200 }}
+									aria-label='criterios dissertativos'
 								>
-									<TableCell
-										component='th'
-										scope='row'
-										align='left'
-										width='50%'
-										style={{ fontWeight: 600 }}
-									>
-										Apresentação e consistência
-									</TableCell>
-									<TableCell>{data.apresentacao}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Relevância e contribuição
-									</TableCell>
-									<TableCell>{data.relevancia}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>Metodologia</TableCell>
-									<TableCell>{data.metodologia}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Adequação ao curso
-									</TableCell>
-									<TableCell>
-										{data.foiAdequado ? '1 - Adequado' : '2 - Inadequado'}
-									</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Plano de trabalho a ser desenvolvido
-									</TableCell>
-									<TableCell>{data.planoDeTrabalho}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Avaliação geral
-									</TableCell>
-									<TableCell>
-										{data.foiAprovado
-											? '1 - Proposta Aprovada'
-											: '2 - Proposta Reprovada'}
-									</TableCell>
-								</TableRow>
-							</TableBody>
-						</Table>
-					</TableContainer>
+									<TableBody>
+										<TableRow
+											key='Resumo da proposta'
+											sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+										>
+											<TableCell
+												component='th'
+												scope='row'
+												align='left'
+												width='50%'
+												style={{ fontWeight: 600 }}
+											>
+												Resumo da proposta
+											</TableCell>
+											<TableCell>{data.summary}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Pontos fortes da proposta
+											</TableCell>
+											<TableCell>{data.strengths}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Pontos fracos da proposta
+											</TableCell>
+											<TableCell>{data.weaknesses}</TableCell>
+										</TableRow>
+										<TableRow>
+											<TableCell style={{ fontWeight: 600 }}>
+												Detalhamento da avaliação
+											</TableCell>
+											<TableCell>{data.details}</TableCell>
+										</TableRow>
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</div>
+					</div>
 				</div>
-				<div className='dissertative-criteria'>
-					<h3>Critérios Dissertativos</h3>
-					<TableContainer component={Paper}>
-						<Table sx={{ width: 1200 }} aria-label='criterios dissertativos'>
-							<TableBody>
-								<TableRow
-									key='Resumo da proposta'
-									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-								>
-									<TableCell
-										component='th'
-										scope='row'
-										align='left'
-										width='50%'
-										style={{ fontWeight: 600 }}
-									>
-										Resumo da proposta
-									</TableCell>
-									<TableCell>{data.resumo}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Pontos fortes da proposta
-									</TableCell>
-									<TableCell>{data.pontosFortes}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Pontos fracos da proposta
-									</TableCell>
-									<TableCell>{data.pontosFracos}</TableCell>
-								</TableRow>
-								<TableRow>
-									<TableCell style={{ fontWeight: 600 }}>
-										Detalhamento da avaliação
-									</TableCell>
-									<TableCell>{data.detalhamento}</TableCell>
-								</TableRow>
-							</TableBody>
-						</Table>
-					</TableContainer>
+			) : (
+				<div className='show-revision'>
+					<div className='show-revision-title'>
+						<h2>Revisores</h2>
+					</div>
+					<div className='show-revision-content'>
+						<div className='reviewers'>
+							<div className='reviewers-title'>
+								<h3>Revisor</h3>
+								<h3>Revisado em</h3>
+							</div>
+
+							<TableContainer component={Paper}>
+								<Table sx={{ width: 1200 }} aria-label='revisores'>
+									<TableBody>
+										{data.map((revisao) => (
+											<TableRow>
+												<TableCell
+													component='th'
+													scope='row'
+													align='left'
+													width='50%'
+													style={{ fontWeight: 600 }}
+												>
+													{revisao.reviewer}
+												</TableCell>
+												<TableCell>
+													{format(new Date(revisao.reviewedOn), 'dd/MM/yyyy')}
+												</TableCell>
+											</TableRow>
+										))}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	)
 }
 
--- src/containers/Student/ViewRevision/index.js
+++ src/containers/Student/ViewRevision/index.js
@@ -3,44 +3,55 @@ import { Header, ShowRevision } from '../../../components'
 import { Button } from '@material-ui/core'
 import './style.css'
 
-let revisoesData = [
-	{
-		id: 1,
-		propostaId: 1,
-		revisadoEm: '16-04-2023',
-		apresentacao: '3 - Bom',
-		relevancia: '4 - Excelente',
-		metodologia: '3 - Bom',
-		foiAdequado: true,
-		planoDeTrabalho: '2 - Exequível',
-		foiAprovado: true,
-		resumo:
-			'O trabalho trata sobre xxxx. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-		pontosFortes: 'Os pontos fortes são que blablabla.',
-		pontosFracos: 'Como pontos fracos é possivel citar xlaxaoxalxoalo.',
-		detalhamento: 'Detalhes não sei pois não li tudo.',
-		numeroServidorRevisor: 1234
-	},
-	{
-		id: 2,
-		propostaId: 1,
-		revisadoEm: '16-04-2023',
-		apresentacao: '2 - Regular',
-		relevancia: '3 - Bom',
-		metodologia: '1 - Insuficiente',
-		foiAdequado: false,
-		planoDeTrabalho: '1 - Mal especificado e/ou não exequível',
-		foiAprovado: false,
-		resumo:
-			'O trabalho trata sobre um bagulho interessante mas foi mal feito. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-		pontosFortes: 'Os pontos fortes são o assunto né mas ta uma bosta.',
-		pontosFracos: 'Os pontos fracos são todo o resto.',
-		detalhamento: 'Nao sei detalhes pois li rapidao e nao entendi nada.',
-		numeroServidorRevisor: 5678
-	}
-]
-
-let propostaTitle = 'Proposta Teste'
+let revisoesData = {
+	id: 1,
+	title:
+		'Decisão de modo de codificação de nuvens de pontos segundo o padrão G-PCC utilizando aprendizado de máquina',
+	reviews: [
+		{
+			id: 1,
+			proposalId: 1,
+			reviewedOn: '2021-03-05T03:00:00.000Z',
+			presentation: '2 - Regular',
+			relevance: '3 - Bom',
+			methodology: '2 - Regular',
+			wasAdequate: true,
+			workPlan: '2 - Exequível',
+			wasApproved: false,
+			summary:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget auctor erat, a lacinia tellus. Mauris rutrum, leo eget tempor vulputate, diam magna consequat nunc, at lacinia nulla augue sed magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam cursus quam nec pellentesque tristique. Integer sed sem ultricies, pellentesque turpis et, convallis urna. Pellentesque iaculis sem sagittis sapien congue pharetra. Nunc varius nunc ut ipsum facilisis commodo. Sed vestibulum, ipsum ac mattis facilisis, velit ex dignissim dui, id commodo purus turpis et risus. Aenean malesuada, felis at pharetra porta, nibh ligula faucibus nunc, ac feugiat nisl erat vel ante. Nunc mattis, risus pretium ultricies interdum, nibh lectus posuere erat, ut dignissim lacus dui in massa. Phasellus eleifend diam at sem blandit, sit amet consectetur massa fringilla. Aenean pretium aliquam nisi nec blandit. Proin semper, dui non fringilla ultrices, nibh dolor mollis justo, posuere feugiat neque nisi ac massa. Donec fermentum consectetur orci, ac imperdiet mauris imperdiet eget. Proin augue enim, porttitor nec nulla eu, eleifend mollis nunc.\n     Praesent sit amet bibendum metus, id accumsan nibh. Pellentesque convallis odio eget pharetra euismod. Duis blandit arcu eget ultrices egestas. Nulla ac aliquet libero. Vivamus scelerisque pharetra dui, tempor egestas tortor rhoncus ac. Mauris porttitor consequat ante, quis porttitor nisl vestibulum quis. Duis vel lacus et orci pellentesque dignissim. Praesent sollicitudin tempus vestibulum. Ut sollicitudin posuere velit, vel hendrerit turpis facilisis ut. Aliquam sit amet eros id risus maximus facilisis. Donec in velit vitae dolor fringilla ullamcorper non eget massa. Ut efficitur iaculis diam, vel ornare erat fringilla lobortis. Vestibulum sodales accumsan finibus. Sed in felis non nunc mattis tincidunt vitae eu ipsum. Phasellus venenatis massa tellus, ac dapibus turpis tempor id.\n     Donec sit amet orci vulputate turpis tristique molestie.',
+			strengths:
+				'Pellentesque convallis odio eget pharetra euismod. Duis blandit arcu eget ultrices egestas. Nulla ac aliquet libero. Vivamus scelerisque pharetra dui, tempor egestas tortor rhoncus ac. Mauris porttitor consequat ante, quis porttitor nisl vestibulum quis. Duis vel lacus et orci pellentesque dignissim. ',
+			weaknesses:
+				'Nulla ac aliquet libero. Vivamus scelerisque pharetra dui, tempor egestas tortor rhoncus ac. Mauris porttitor consequat ante, quis porttitor nisl vestibulum quis. Duis vel lacus et orci pellentesque dignissim.',
+			details:
+				'Donec in velit vitae dolor fringilla ullamcorper non eget massa. Ut efficitur iaculis diam, vel ornare erat fringilla lobortis.',
+			reviewerEmail: 'ldsfreitas@inf.ufpel.edu.br',
+			reviewer: 'Larissa Astrogildo de Freitas'
+		},
+		{
+			id: 2,
+			proposalId: 1,
+			reviewedOn: '2021-04-06T03:00:00.000Z',
+			presentation: '4 - Excelente',
+			relevance: '3 - Bom',
+			methodology: '3 - Bom',
+			wasAdequate: true,
+			workPlan: '2 - Exequível',
+			wasApproved: true,
+			summary:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget auctor erat, a lacinia tellus. Mauris rutrum, leo eget tempor vulputate, diam magna consequat nunc, at lacinia nulla augue sed magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam cursus quam nec pellentesque tristique. Integer sed sem ultricies, pellentesque turpis et, convallis urna. Pellentesque iaculis sem sagittis sapien congue pharetra. Nunc varius nunc ut ipsum facilisis commodo. Sed vestibulum, ipsum ac mattis facilisis, velit ex dignissim dui, id commodo purus turpis et risus. Aenean malesuada, felis at pharetra porta, nibh ligula faucibus nunc, ac feugiat nisl erat vel ante. Nunc mattis, risus pretium ultricies interdum, nibh lectus posuere erat, ut dignissim lacus dui in massa. Phasellus eleifend diam at sem blandit, sit amet consectetur massa fringilla. Aenean pretium aliquam nisi nec blandit. Proin semper, dui non fringilla ultrices, nibh dolor mollis justo, posuere feugiat neque nisi ac massa. Donec fermentum consectetur orci, ac imperdiet mauris imperdiet eget. Proin augue enim, porttitor nec nulla eu, eleifend mollis nunc.\n    Praesent sit amet bibendum metus, id accumsan nibh. Pellentesque convallis odio eget pharetra euismod. Duis blandit arcu eget ultrices egestas. Nulla ac aliquet libero. Vivamus scelerisque pharetra dui, tempor egestas tortor rhoncus ac. Mauris porttitor consequat ante, quis porttitor nisl vestibulum quis. Duis vel lacus et orci pellentesque dignissim. Praesent sollicitudin tempus vestibulum. Ut sollicitudin posuere velit, vel hendrerit turpis facilisis ut. Aliquam sit amet eros id risus maximus facilisis. Donec in velit vitae dolor fringilla ullamcorper non eget massa. Ut efficitur iaculis diam, vel ornare erat fringilla lobortis. Vestibulum sodales accumsan finibus. Sed in felis non nunc mattis tincidunt vitae eu ipsum. Phasellus venenatis massa tellus, ac dapibus turpis tempor id.\n    Donec sit amet orci vulputate turpis tristique molestie.',
+			strengths:
+				'Pellentesque convallis odio eget pharetra euismod. Duis blandit arcu eget ultrices egestas. Nulla ac aliquet libero. Vivamus scelerisque pharetra dui, tempor egestas tortor rhoncus ac. Mauris porttitor consequat ante, quis porttitor nisl vestibulum quis. Duis vel lacus et orci pellentesque dignissim. ',
+			weaknesses:
+				'Nulla ac aliquet libero. Vivamus scelerisque pharetra dui, tempor egestas tortor rhoncus ac. Mauris porttitor consequat ante, quis porttitor nisl vestibulum quis. Duis vel lacus et orci pellentesque dignissim.',
+			details:
+				'Donec in velit vitae dolor fringilla ullamcorper non eget massa. Ut efficitur iaculis diam, vel ornare erat fringilla lobortis.',
+			reviewerEmail: 'ldsfreitas@inf.ufpel.edu.br',
+			reviewer: 'Rafael Piccin Torchelsen'
+		}
+	]
+}
 
 const ViewRevision = () => {
 	return (
@@ -48,11 +59,15 @@ const ViewRevision = () => {
 			<Header />
 			<div className='view-revision-title'>
 				<h1>Revisão de proposta</h1>
-				<h3>Proposta: {propostaTitle}</h3>
+				<h3>Proposta: {revisoesData.title}</h3>
 			</div>
 			<div className='view-revision-box'>
-				{revisoesData.map((revisao) => (
-					<ShowRevision data={revisao} />
+				<div className='view-revision-reviewers'>
+					<ShowRevision isRevision={false} data={revisoesData.reviews} />
+				</div>
+
+				{revisoesData.reviews.map((revisao) => (
+					<ShowRevision isRevision={true} data={revisao} />
 				))}
 			</div>
 			<div className='back-button'>
