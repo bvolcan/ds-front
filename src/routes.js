54529b756ab01d8396e3641ec65bc274a8197c2c
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2023-04-24 16:59:05 -0300

    refac: put viewclass from advisor back in routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
 import Login from './containers/Login'
 import ChangePassword from './containers/Login/ChangePassword'
 import StudentList from './containers/Student/List'
 import ViewRevision from './containers/Student/ViewRevision'
+import ViewClass from './containers/Advisor/ViewClass'
 
 const Routes = () => {
 	return (
@@ -12,7 +12,7 @@ const Routes = () => {
 			<Switch>
 				<Route component={StudentList} path='/studentlist' />
 				<Route component={ViewRevision} path='/viewrevision' />
-
+				<Route component={ViewClass} path='/advisorviewclass' />
 				<Route component={ChangePassword} path='/changepassword' />
 				<Route component={Login} path='/' />
 			</Switch>
