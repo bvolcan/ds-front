d8a764fb8403335a3b26762e55c812809eea8ee2
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2023-04-26 21:11:53 -0300

    feat: added components with index

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Header } from './Header/Header'
+export { default as ClassesContainer } from './ClassesContainer/'
+export { default as CoordinatorClassesContainer } from './CoordinatorClassesContainer/'
