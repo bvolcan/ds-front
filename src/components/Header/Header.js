69f6a5b1ca2a8b836ee102ba3137476873c2cb7b
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2023-05-09 20:10:35 -0300

    mexidinha no header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,14 @@ import React from 'react'
 import './Header.css'
 import Larichair_Icon from '../../images/larichair_icon.png'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return (
 		<header>
-			<img src={Larichair_Icon} alt=''></img>
+			<Link to='/'>
+				<img src={Larichair_Icon} alt=''></img>
+			</Link>
 			<div className='profile-pic'>
 				<AccountCircleIcon sx={{ fontSize: 40 }} />
 			</div>
